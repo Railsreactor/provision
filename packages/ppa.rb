d695fc4121d914cf074a0a48dc4f4c59f95b0875
dmishh <dmishh@gmail.com>
2015-03-04 00:11:25 +0200

    Added software-properties-common package to get add-apt-repository working See http://lifeonubuntu.com/ubuntu-missing-add-apt-repository-command/

--- packages/ppa.rb
+++ packages/ppa.rb
@@ -1,7 +1,10 @@
 package :ppa do
-  apt 'python-software-properties'
+  apt 'python-software-properties', 'software-properties-common' do
+    pre :install, ['aptitude update']
+  end
 
   verify do
     has_apt 'python-software-properties'
+    has_apt 'software-properties-common'
   end
 end
