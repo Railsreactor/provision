387f1817feb5c9fd1bef642fa79fc9d0991280b6
dmishh <dmishh@gmail.com>
2015-03-09 13:05:35 +0200

    Added security_upgrades package, which sets up automatic daily security upgrades Also see https://help.ubuntu.com/lts/serverguide/automatic-updates.html

---
+++ packages/security_upgrades.rb
@@ -0,0 +1,28 @@
+# see https://help.ubuntu.com/lts/serverguide/automatic-updates.html
+package :security_upgrades do
+  description 'Automatic security upgrades'
+
+  requires :unattended_upgrades
+
+  %w{10periodic 50unattended-upgrades}.each do |config_file|
+    config_file_path = "/etc/apt/apt.conf.d/#{config_file}"
+    config_template = File.join(File.dirname(__FILE__), 'configs', config_file)
+
+    file config_file_path, contents: File.read(config_template), sudo: true, owner: 'root:root', mode: '644'
+  end
+
+  verify do
+    has_file '/etc/apt/apt.conf.d/10periodic'
+    has_file '/etc/apt/apt.conf.d/50unattended-upgrades'
+  end
+end
+
+package :unattended_upgrades do
+  apt 'unattended-upgrades' do
+    pre :install, ['aptitude update']
+  end
+
+  verify do
+    has_apt 'unattended-upgrades'
+  end
+end
