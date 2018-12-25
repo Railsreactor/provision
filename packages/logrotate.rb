6bf1a38b02960721f639ceeaab9236c7ece1319c
rilian <dmitriyis@gmail.com>
2018-12-25 11:29:57 -0500

    added sample logrotate setup commands

--- packages/logrotate.rb
+++ packages/logrotate.rb
@@ -1,3 +1,26 @@
+# Sample setup for Rails app
+#
+# ssh ...
+#
+# sudo su
+#
+# cd /home/ubuntu/xxx/shared/log && ls -la && vi /etc/logrotate.d/xxx.conf
+#
+# /home/ubuntu/xxx/current/log/*.log {
+#  su root
+#  daily
+#  maxsize 10M
+#  missingok
+#  notifempty
+#  rotate 14
+#  compress
+#  delaycompress
+#  copytruncate
+#  nodateext
+#}
+#
+# logrotate /etc/logrotate.d/xxx.conf && ls -la /home/ubuntu/xxx/shared/log
+#
 package :logrotate do
   description 'Install logrotate'
 
